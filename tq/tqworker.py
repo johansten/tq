ec78ceeaf7df102a258f93ad34b89cde069e0d07
Johan Stén <johan.sten@gmail.com>
2012-08-30 17:43:34 +0800

    Changed the Worker constructor to take a redis instance and a list of queues. Moved some other stuff into the Worker class.

--- tq/tqworker.py
+++ tq/tqworker.py
@@ -14,10 +14,12 @@ import traceback
 
 class Worker(object):
 	
-	def __init__(self):
-		self.hash = os.urandom(8).encode('hex')
+	def __init__(self, r, queues):
+		self.hash	= os.urandom(8).encode('hex')
+		self.r		= r
+		self.queues = queues
 
-	def register(self, queues):
+	def register(self):
 		dict = {}
 		name = socket.gethostname()
 		if name.endswith('.local'):
@@ -27,91 +29,93 @@ class Worker(object):
 		dict['name'] = name
 		dict['pid']  = pid
 	
-		r.hmset('tq:worker:%s' % self.hash, dict)
-		r.sadd('tq:workers', self.hash)
-		for q in queues:
-			r.sadd('tq:queue:%s:workers' % q, self.hash)
+		self.r.hmset('tq:worker:%s' % self.hash, dict)
+		self.r.sadd('tq:workers', self.hash)
+		for q in self.queues:
+			self.r.sadd('tq:queue:%s:workers' % q, self.hash)
 
-	def unregister(self, queues):
+	def unregister(self):
 
 		# push any unfinished tasks on failed
 
-		id = r.hget('tq:worker:%s' % self.hash, 'task')
+		id = self.r.hget('tq:worker:%s' % self.hash, 'task')
 		if id is not None:
 			tb = traceback.format_exc()
-			r.hset('tq:task:%s' % id, 'traceback', tb)
-			r.rpush('tq:queue:failed:fifo', id)
+			self.r.hset('tq:task:%s' % id, 'traceback', tb)
+			self.r.rpush('tq:queue:failed:fifo', id)
 
 		# unregister worker		
 
-		for q in queues:
-			r.srem('tq:queue:%s:workers' % q, self.hash)
-		r.srem('tq:workers', self.hash)
-		r.delete('tq:worker:%s' % self.hash)
+		for q in self.queues:
+			self.r.srem('tq:queue:%s:workers' % q, self.hash)
+		self.r.srem('tq:workers', self.hash)
+		self.r.delete('tq:worker:%s' % self.hash)
 
-#-------------------------------------------------------------------------------
+	def perform_task(self, id):
 
-def work(worker, id):
+		try:
+			dict = self.r.hgetall('tq:task:%s' % id)
+			func_name, args, kwargs = pickle.loads(dict['data'])
 
-	try:
-		dict = r.hgetall('tq:task:%s' % id)
-		func_name, args, kwargs = pickle.loads(dict['data'])
+			module_name, func_name = func_name.rsplit('.', 1)
+			module = importlib.import_module(module_name)
 
-		module_name, func_name = func_name.rsplit('.', 1)
-		module = importlib.import_module(module_name)
+		#	arg_list = [repr(arg) for arg in args]
+		#	arg_list += ['%s=%r' % (k, v) for k, v in kwargs.items()]
+		#	arguments = ', '.join(arg_list)
+		#	print '%s(%s)' % (func_name, arguments)
 
-	#	arg_list = [repr(arg) for arg in args]
-	#	arg_list += ['%s=%r' % (k, v) for k, v in kwargs.items()]
-	#	arguments = ', '.join(arg_list)
-	#	print '%s(%s)' % (func_name, arguments)
+			now = int(time.time())
+			self.r.hset('tq:task:%s' % id, 'started', now)
+			self.r.hset('tq:worker:%s' % self.hash, 'task', id)
 
-		now = int(time.time())
-		r.hset('tq:task:%s' % id, 'started', now)
-		r.hset('tq:worker:%s' % worker, 'task', id)
+			result = getattr(module, func_name)(*args, **kwargs)
 
-		result = getattr(module, func_name)(*args, **kwargs)
-	except Exception:
-		tb = traceback.format_exc()
-		r.hset('tq:task:%s' % id, 'traceback', tb)
-		r.rpush('tq:queue:failed', id)
-	else:
-		r.hset('tq:task:%s' % id, 'result', result)
+		except Exception:
+			tb = traceback.format_exc()
+			self.r.hset('tq:task:%s' % id, 'traceback', tb)
+			self.r.rpush('tq:queue:failed', id)
 
-	r.hdel('tq:worker:%s' % worker, 'task')
-	return True
+		else:
+			self.r.hset('tq:task:%s' % id, 'result', result)
+
+		self.r.hdel('tq:worker:%s' % self.hash, 'task')
+		return True
+
+	def work(self):
+
+		self.register()
+
+		try:
+			while True:
+				for q in self.queues:
+					task = tq.taskqueue.dequeue(q)
+					if task is not None:
+						self.perform_task(task)
+						break
+				time.sleep(1)
+
+		except KeyboardInterrupt:
+			self.unregister()
 
 #-------------------------------------------------------------------------------
 
 def main():
-	if 0:
+
+	r = redis.Redis(host='localhost')
+
+	if 1:
 		keys = r.keys('tq:*')
 		for k in keys:
 			r.delete(k)
-		return
 
 	#	register worker
 
 	queues = ['high', 'default', 'low']
-	worker = Worker()
-	worker.register(queues)
-
-	#	work
-
-	try:
-		while True:
-			for q in queues:
-				task = tq.taskqueue.dequeue(q)
-				if task is not None:
-					work(worker.hash, task)
-					break
-			time.sleep(1)
-
-	except KeyboardInterrupt:
-		worker.unregister(queues)
+	worker = Worker(r, queues)
+	worker.work()
 
 #-------------------------------------------------------------------------------
-
-r = redis.Redis(host='localhost')
 
 if __name__ == '__main__':
 	main()
